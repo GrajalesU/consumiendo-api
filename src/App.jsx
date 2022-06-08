df4249e6077c24e6bac377d297d292d016f9d0fc
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-06-08 16:28:36 -0500

    Mejorando diseño

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,85 @@
-import { useState } from 'react'
+import {useState} from 'react';
 
-import './App.css'
+import './App.css';
 
 function App() {
-  const [query, setQuery] = useState('')
-  const [results, setResults] = useState([])
-  const [page, setPage] = useState(undefined)
-  const [totalPages, setTotalPages] = useState(undefined)
-  const [loading, setLoading] = useState(false)
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+  const [page, setPage] = useState(undefined);
+  const [totalPages, setTotalPages] = useState(undefined);
+  const [loading, setLoading] = useState(false);
 
   const handleSearch = () => {
-    if(query === '') {
-      setResults([])
-      alert("No has escrito nada en el buscador")
-      return
+    if (query === '') {
+      setResults([]);
+      alert('No has escrito nada en el buscador');
+      return;
     }
-    setLoading(true)
-    fetch("https://api.mercadolibre.com/sites/MCO/search?q="+query).then(res => res.json()).then(data => {
-      setResults(data.results)
-      setTotalPages(data.paging.limit)
-      setPage(0)
-      setLoading(false)
-    })
-  }
+    setLoading(true);
+    fetch('https://api.mercadolibre.com/sites/MCO/search?q='+query).then((res) => res.json()).then((data) => {
+      setResults(data.results);
+      setTotalPages(data.paging.limit);
+      setPage(0);
+      setLoading(false);
+    });
+  };
 
   const handlePage = (page) => {
-    setPage(page)
-    setLoading(true)
-    fetch("https://api.mercadolibre.com/sites/MCO/search?q="+query+"&offset="+page).then(res => res.json()).then(data => {
-      setResults(data.results)
-      setLoading(false)
-    })
-  }
+    setPage(page);
+    setLoading(true);
+    fetch('https://api.mercadolibre.com/sites/MCO/search?q='+query+'&offset='+page).then((res) => res.json()).then((data) => {
+      setResults(data.results);
+      setLoading(false);
+    });
+  };
 
   return (
     <div className="App">
-      <div className="Searcher">
-      <p>Juan Manuel Grajales</p>
-      <input type="text" placeholder='Buscar elemento en mercadolibre CO' value={query} onChange={(event)=>{
-        setQuery(event.target.value)
-      }}/>
-      <button type="button" onClick={handleSearch}>buscar</button>
-      </div>
-      {    totalPages &&  
-      <div className="Paginator">
-        {page > 0 && 
-          <button type="button" onClick={()=>{handlePage(page-1)}}>Anterior</button>}
-          <p>{page}</p>
-        {page < totalPages &&
-          <button type="button" onClick={()=>{handlePage(page+1)}}>Siguiente</button>}
-      </div>}
-      {loading ? 
-      <p>Cargando ...</p> :
+      <header>
+        <div className="Searcher">
+          <p>Juan Manuel <br/> Grajales</p>
+          <div className="searchBar">
+            <input type="text"
+              placeholder='Buscar elemento en mercadolibre CO'
+              value={query}
+              onChange={(event)=>{
+                setQuery(event.target.value);
+              }}/>
+            <button type="button" onClick={handleSearch}>🔍</button>
+          </div>
+        </div>
+      </header>
+      <main>
+        {loading ?
+      <p className='loader'>Cargando ...</p> :
       <div className="Results">
-        {results.map(result => (
+        {results.map((result) => (
           <div className='Card' key={result.id}>
             <img src={result.thumbnail} alt={result.title}/>
             <div className='description'>
-              <p>{result.title}</p>
-              <p>{result.price} COP</p>
-              <a href={result.permalink}>Ver producto</a>
+              <p>$ {new Intl.NumberFormat('cop').format(result.price)}</p>
+              <p className='title'>{result.title}</p>
             </div>
           </div>))}
       </div>}
-      
+      </main>
+      { totalPages &&
+      <div className="Paginator">
+        {page > 0 &&
+          <button type="button" onClick={()=>{
+            handlePage(page-1);
+          }}>↽ Anterior</button>}
+        <p><b>{page}</b> de {totalPages}</p>
+        {page < totalPages &&
+          <button type="button" onClick={()=>{
+            handlePage(page+1);
+          }}>Siguiente ⇀</button>}
+      </div>}
+
+      <div>Font made from <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>is licensed by CC BY 3.0</div>
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
